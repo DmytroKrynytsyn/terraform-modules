2aca55a023c23de468d85f3555e1080e3e785d34
Dmytro Krynytsyn <dmitry.krinitsyn@gmail.com>
2025-02-06 19:23:21 +0100

    fix redis, adding output variables

--- configuration/redis-cluster/outputs.tf
+++ configuration/redis-cluster/outputs.tf
@@ -1,7 +1,19 @@
-output "kafka_brokers_private_ips" {
-    value = module.asg-ec2.private_ips
+output "redis_primary_instance_public_ip" {
+  description = "Redis primary public IP"
+  value       = aws_instance.redis_primary.public_ip
 }
 
-output "kafka_brokers_public_ips" {
-    value = module.asg-ec2.public_ips
+output "redis_primary_instance_private_ip" {
+  description = "Redis primary private IP"
+  value       = aws_instance.redis_primary.private_ip
+}
+
+output "redis_secondary_instance_public_ip" {
+  description = "Redis secondary public IP"
+  value       = aws_instance.redis_secondary.public_ip
+}
+
+output "redis_secondary_instance_private_ip" {
+  description = "Redis secondary private IP"
+  value       = aws_instance.redis_secondary.private_ip
 }
