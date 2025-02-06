8bc8fa530762966d8f0b5333619ae54778639a48
Dmytro Krynytsyn <dmitry.krinitsyn@gmail.com>
2025-02-06 18:48:04 +0100

    remove test node

--- configuration/elasticsearch-fluentd-kibana-cluster/outputs.tf
+++ configuration/elasticsearch-fluentd-kibana-cluster/outputs.tf
@@ -14,12 +14,6 @@ output "kibana_instance_public_ip" {
   value       = aws_instance.kibana.public_ip
 }
 
-output "node_instance_public_ip" {
-  description = "Node public IP"
-  value       = aws_instance.node.public_ip
-}
-
-
 output "elasticsearch_instance_private_ip" {
   description = "Elasticsearch private IP"
   value       = aws_instance.elasticsearch.private_ip
@@ -33,9 +27,4 @@ output "fluentd_instance_private_ip" {
 output "kibana_instance_private_ip" {
   description = "Kibana private IP"
   value       = aws_instance.kibana.private_ip
-}
-
-output "node_instance_private_ip" {
-  description = "Node private IP"
-  value       = aws_instance.node.private_ip
 }
